122d6d57c7edc56f547d8187391411811ef88313
｛Otsuka｝ <21411052ho@tama.ac.jp>
2016-06-23 15:23:01 +0900

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float jumpPower;
+	public GameObject Score;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")){
+			GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+			}
+
+    }
+	 
+	void OnCollisionEnter (Collision Collision) {
+		Application.LoadLevel("GameOver");
+	}
+
+	void OnTriggerEnter (Collider col){
+		//Score(GUIText)のScoreUpメソッドを呼び出す
+		Score.SendMessage("ScoreUp", 1);
+		Destroy(col.gameObject);
+	}
+
+
+
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	private int score = 0;
+	
+	void Start () {
+		GetComponent<Text>().text = "Score: " + score.ToString();
+	}
+	
+	public void ScoreUp (int point)
+	{
+		score += point;
+		GetComponent<Text> ().text = "Score: " + score.ToString ();
+	}
+
+
+}
